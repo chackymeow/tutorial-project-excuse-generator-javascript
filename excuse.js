2d8bc48cc18a70fb6938819d59e43d882b710de1
chackymeow <fabichasola@hotmail.com>
2021-02-06 01:46:32 +0000

    avance

---
+++ excuse.js
@@ -0,0 +1,15 @@
+let who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'the keys', 'the car'];
+let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
+
+let random = 0; let random1 = 0; let random2 = 0; let random3 = 0;
+
+random = Math.floor(Math.random()*4);
+random1 = Math.floor(Math.random()*4);
+random2 = Math.floor(Math.random()*3);
+random3 = Math.floor(Math.random()*4);
+
+final = who[random] +" "+ action[random1] +" "+ what[random2] +" "+ when[random3];
+//alert(final);
+document.getElementById("enviar").innerHTML = final;
